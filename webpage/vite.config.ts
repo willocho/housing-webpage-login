ed3f4fb510554a7a52f54e7fc5f370b2f0e53ec0
Will Ochowicz <will.ochowicz@genusplc.com>
2025-10-04 11:32:16 -0500

    Remove rewrite from vite.config.ts

--- webpage/vite.config.ts
+++ webpage/vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
         target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
